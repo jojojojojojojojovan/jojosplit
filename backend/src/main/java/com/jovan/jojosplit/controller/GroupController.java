0548a28b0a04bfe1c93f7ca680db895fa23cc8ea
Jovan <teo.jovan98@gmail.com>
2026-05-15 17:21:22 +0800

    Add cors to backend groupcontroller

--- backend/src/main/java/com/jovan/jojosplit/controller/GroupController.java
+++ backend/src/main/java/com/jovan/jojosplit/controller/GroupController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/groups")
+@CrossOrigin(origins = "http://localhost:5173")
 public class GroupController {
     
     private final GroupService groupService;
